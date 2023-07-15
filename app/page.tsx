579dc91614c700ddebb950135e57d5242f6e2aea
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-07-15 16:53:09 +0600

    basic layout added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import Hero from "@/components/Hero";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <main className="mt-20">
+      <Hero />
+    </main>
+  );
 };
 
 export default HomePage;
