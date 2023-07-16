6517244d1873f7d1fcc00fe0bc204e66c30656ec
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-07-17 01:31:12 +0600

    navbar added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,46 @@
+import Link from "next/link";
+import Button from "./Button";
+
 const Navbar = () => {
   return (
-    <header className="h-20 fixed top-0 left-0 right-0 bg-rose-500">
-      Navbar
+    <header className="h-20 fixed top-0 left-0 right-0 bg-light/90 backdrop-blur-lg border border-b border-dark/10 z-[100]">
+      <div className="wrapper w-full h-full flex justify-between items-center">
+        {/* NAV LEFT */}
+        <nav>
+          <Link href="/" className="text-xl font-semibold link-item">
+            Dazzle Mart
+          </Link>
+        </nav>
+        {/* NAV MID */}
+        <nav>
+          <ul className="flex gap-5">
+            <li>
+              <Link href="/" className="link-item">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/products" className="link-item">
+                Products
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" className="link-item">
+                ABout
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" className="link-item">
+                Contaact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        {/* NAV RIGHT */}
+        <nav>
+          <Button href="/login" placeholder="Login" color="green" />
+        </nav>
+      </div>
     </header>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      backgroundImage: {},
       colors: {
         light: "#F4F4F4",
         dark: "#21242B",
